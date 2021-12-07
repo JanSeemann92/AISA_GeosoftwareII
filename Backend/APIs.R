5fee89732b803dc7c59560ff264911036bb41f24
Jan Seemann <52669713+JanSeemann92@users.noreply.github.com>
2021-12-07 16:34:52 +0100

    API Overview
    gathering needed APis

---
+++ Backend/APIs.R
@@ -0,0 +1,40 @@
+#APIs
+
+library(beakr)
+
+
+#POST-Request (without trained model)
+
+
+
+
+
+#output job number ?
+#----------------------------------
+
+#POST-Request (with trained model)
+
+
+#output job number?
+#------------------------------
+
+
+#get-Request (visualisation)
+
+#----------------------------
+
+
+
+#get-Request (download AOA)
+
+#-----------------------------------
+
+
+#get-Request (download sampling areas)
+
+#--------------------------------------
+
+
+#get-Request (download trained model)
+
+#---------------------------------------
