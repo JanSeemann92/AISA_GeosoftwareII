17c8d0d3d1e6971a184536761ac20756ece4c4de
Jan Seemann <52669713+JanSeemann92@users.noreply.github.com>
2021-12-07 18:36:32 +0100

    API Specification
    added (part of) API Specification

--- Backend/APIs.R
+++ Backend/APIs.R
@@ -20,21 +20,33 @@ library(beakr)
 
 
 #get-Request (visualisation)
+#input job number?
 
+
+# AOA as geoJSON or other for Leaflet usable format
 #----------------------------
 
 
 
 #get-Request (download AOA)
+#input job number?
 
+
+#output AOA as GeoTIFF
 #-----------------------------------
 
 
 #get-Request (download sampling areas)
+#input job number?
 
+#output sampling area as geoJSON
 #--------------------------------------
 
 
 #get-Request (download trained model)
+#input job number?
+
+#output trained model as .rdat or JSON
+
 
 #---------------------------------------
