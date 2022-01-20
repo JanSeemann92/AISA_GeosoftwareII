72c511208c233d0eb10ed83fadb59c26062ce6ea
KatharinaGI <katharina.m.kaufmann@web.de>
2022-01-20 20:16:11 +0100

    continued work on handover test

--- public/javascripts/2_calculateAOA.js
+++ public/javascripts/2_calculateAOA.js
@@ -92,6 +92,14 @@ slider.oninput = function() {
 } 
 
 function startCalculation(){
+  var state = document.querySelector("#choose2").options.selectedIndex;
+    console.log(state)
+  if(state == 0){
+    document.querySelector('#msgoption').style.display = 'block';
+  }
+  else{
+  if(state == 2){
+  document.querySelector('#msgoption').style.display = 'none';
   if(document.querySelector('#validate3').style.display == 'block'){
     if (document.querySelector('#validate2').checked == false){  
       console.log('Test3: false')}
@@ -107,7 +115,8 @@ function startCalculation(){
       console.log(xmin);
       console.log(ymax);
       console.log(xmax);
-      console.log(slider.value)
+      var cloudcover = slider.value;
+      console.log(cloudcover);
       var resolution = document.getElementById('resolution').value;
       console.log(resolution);
       //alert("The calculation will now be executed, you will then be redirected to the results. The calculation may take a few minutes, please wait...")
@@ -125,7 +134,8 @@ function startCalculation(){
       console.log(xmin);
       console.log(ymax);
       console.log(xmax);
-      console.log(slider.value)
+      var cloudcover = slider.value;
+      console.log(cloudcover);
       var resolution = document.getElementById('resolution').value;
       console.log(resolution);
       //alert("The calculation will now be executed, you will then be redirected to the results. The calculation may take a few minutes, please wait...")
@@ -136,17 +146,103 @@ function startCalculation(){
     document.querySelector('#validate3').style.display = 'block';
     console.log('Test2: false')}
 }}
+else{
+  document.querySelector('#msgoption').style.display = 'none';
+  console.log('Bounding box!')
+  if(document.querySelector('#validate3').style.display == 'block'){
+    if (document.querySelector('#validate2').checked == false){  
+      console.log('Test3: false')}
+    else{
+      console.log('Test4: true')
+      //coordinates
+      var cloudcover = slider.value;
+      console.log(cloudcover);
+      var resolution = document.getElementById('resolution').value;
+      console.log(resolution);
+      //alert("The calculation will now be executed, you will then be redirected to the results. The calculation may take a few minutes, please wait...")
+      //window.location.href= '/ownresultAOA'
+    }}
+  if(document.querySelector('#validate1').style.display == 'block'){
+    if (document.querySelector('#validate').checked == true){ 
+        //coordinates
+      var cloudcover = slider.value;
+      console.log(cloudcover);
+      var resolution = document.getElementById('resolution').value;
+      console.log(resolution);
+      //alert("The calculation will now be executed, you will then be redirected to the results. The calculation may take a few minutes, please wait...")
+      //window.location.href= '/ownresultAOA'
+    }
+    else{
+    document.querySelector('#validate1').style.display = 'none';
+    document.querySelector('#validate3').style.display = 'block';
+    console.log('Test2: false')}
+}}}}
+
 
 function checkcoordinates(ymin, xmin, ymax, xmax){
   if(ymin == '' || xmin == '' || ymax == '' || xmax == ''){
-    document.querySelector('#msg').style.display = 'block';
+    document.querySelector('#msgmissed').style.display = 'block';
     document.querySelector('#validate2').checked = 'false';
     document.querySelector('#validate').checked = 'false';
     document.querySelector('#validate1').style.display = 'block';
     document.querySelector('#validate3').style.display = 'none';
     return false
 }
+if(checkcoordsformat(ymin, xmin, ymax, xmax) == false){
+  console.log('invalid format')
+}
  else{
-  document.querySelector('#msg').style.display = 'none';
+  document.querySelector('#msgmissed').style.display = 'none';
   document.querySelector('#validate').checked = 'true';
  }}
+
+function checkcoordsformat(ymin, xmin, ymax, xmax){
+  var coords = [ymin, xmin, ymax, xmax];
+  console.log(coords)
+  for(let i=0; i<coords.length; i++){
+    console.log(coords[i])
+    //https://qastack.com.de/programming/18042133/check-if-input-is-number-or-letter-javascript
+    if(isNaN(coords[i])) 
+    {   console.log(isNaN(coords[i]))
+        console.log('No number')
+        document.querySelector('#msgformat').style.display = 'block';
+        document.querySelector('#validate2').checked = 'false';
+        document.querySelector('#validate').checked = 'false';
+        document.querySelector('#validate1').style.display = 'block';
+        document.querySelector('#validate3').style.display = 'none';
+        return false
+    }}
+  var yminfv = ymin.split('.');
+  var ymaxfv = ymax.split('.');
+  if(yminfv[0] > 90 || yminfv[0] <-90 || ymaxfv[0] > 90 || ymaxfv[0] <-90){
+    document.querySelector('#msgformat').style.display = 'block';
+    document.querySelector('#validate2').checked = 'false';
+    document.querySelector('#validate').checked = 'false';
+    document.querySelector('#validate1').style.display = 'block';
+    document.querySelector('#validate3').style.display = 'none';
+    console.log("invalid lat value")
+    return false;
+  }
+  var xminfv = xmin.split('.');
+  var xmaxfv = xmax.split('.');
+  if(xminfv[0] > 180 || xminfv[0] <-180 || xmaxfv[0] > 180 || xmaxfv[0] <-180){
+    document.querySelector('#msgformat').style.display = 'block';
+    document.querySelector('#validate2').checked = 'false';
+    document.querySelector('#validate').checked = 'false';
+    document.querySelector('#validate1').style.display = 'block';
+    document.querySelector('#validate3').style.display = 'none';
+    console.log("invalid lng value")
+    return false;
+  }
+  else{
+    document.querySelector('#msgformat').style.display = 'none';
+    document.querySelector('#validate1').style.display = 'block';
+    document.querySelector('#validate3').style.display = 'none';
+    document.querySelector('#validate').checked = 'true';
+    console.log('valid format')
+    return true
+  }}
+
+
+
+
