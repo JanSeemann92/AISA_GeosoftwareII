ed924771ae0de0da21f51b30ac97a3f1f9a3739d
Antonia <antonia.jost@web.de>
2022-01-29 18:37:26 +0100

    setwd mount volume

--- Backend/calc.R
+++ Backend/calc.R
@@ -41,7 +41,7 @@ library(beakr)
   # library(tmap)
 
 #getwd()
-setwd("/home/ubuntu/AISA_GeosoftwareII/Backend")
+#setwd("/home/ubuntu/AISA_GeosoftwareII/Backend")
 #getwd()
 # set working directory: directory which includes needed data
 #### needs to be changed later on to the hosting server
