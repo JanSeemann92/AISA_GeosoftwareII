ee1d5640555caf086db8a7d67df10d013a2af4b3
Antonia <antonia.jost@web.de>
2022-01-29 11:52:43 +0100

    change IP

--- public/javascripts/3_1_demoresultAOA.js
+++ public/javascripts/3_1_demoresultAOA.js
@@ -16,7 +16,7 @@ var icon = L.icon({
  * function to display the layer on the map.
  */
 
- var url_to_geotiff_file = "http://backend:8782/demodata/createdbyAISAtool/aoaOutput.tif";
+ var url_to_geotiff_file = "http://host.docker.internal:8782/demodata/createdbyAISAtool/aoaOutput.tif";
  fetch(url_to_geotiff_file).then(response => response.arrayBuffer()).then(arrayBuffer => {
     parseGeoraster(arrayBuffer).then(georaster => {
         const min = 0;
@@ -95,7 +95,7 @@ var icon = L.icon({
  */
 
  $.ajax({
-    url: "http://backend:8782/demodata/createdbyAISAtool/labelsOutput.json",
+    url: "http://host.docker.internal:8782/demodata/createdbyAISAtool/labelsOutput.json",
     type: 'GET',
     dataType: 'json', 
     success: function(res) {
@@ -103,7 +103,7 @@ var icon = L.icon({
         console.log(status)
         if(status == 'trainingdata'){
             var xhr = new XMLHttpRequest();
-            xhr.open('GET', "http://backend:8782/demodata/createdbyAISAtool/trainingsitesOutput.geojson");
+            xhr.open('GET', "http://host.docker.internal:8782/demodata/createdbyAISAtool/trainingsitesOutput.geojson");
             xhr.setRequestHeader('Content-Type', 'application/json');
             xhr.onload = function() {
             if (xhr.status === 200) {
@@ -117,7 +117,7 @@ var icon = L.icon({
         var labels = res
         labels.splice(0,1)
         console.log(labels)
-        var url_to_geotiff_file = "http://backend:8782/demodata/createdbyAISAtool/predictionOutput.tif";
+        var url_to_geotiff_file = "http://host.docker.internal:8782/demodata/createdbyAISAtool/predictionOutput.tif";
         fetch(url_to_geotiff_file).then(response => response.arrayBuffer()).then(arrayBuffer => {
             parseGeoraster(arrayBuffer).then(georaster => {
                 const min = 0;
@@ -228,7 +228,7 @@ var icon = L.icon({
  * and then calls the createSamplingLayer(layertraingspots) function to display the layer on the map.
  */
  $.ajax({
-    url: "http://backend:8782/demodata/createdbyAISAtool/samplingLocationsOutput.geojson",
+    url: "http://host.docker.internal:8782/demodata/createdbyAISAtool/samplingLocationsOutput.geojson",
     type: 'GET',
     dataType: 'json',
     success: function(res) {
